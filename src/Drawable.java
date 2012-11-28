881ec19876a3ce3bbda0b39baedfd5b6be6e1223
Andrew Moore <moorea@msoe.edu>
2012-11-28 10:35:12 -0600

    Initial Commit

---
+++ src/Drawable.java
@@ -0,0 +1,20 @@
+/**
+ * @author moorea
+ * @version 1.0
+ * @created 28-Nov-2012 10:09:26 AM
+ */
+public class Drawable {
+
+	public Drawable(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void draw(){
+
+	}
+
+}
---
+++ src/Line.java
@@ -0,0 +1,41 @@
+/**
+ * @author moorea
+ * @version 1.0
+ * @created 28-Nov-2012 10:09:26 AM
+ */
+public class Line extends Shape {
+
+	private int length;
+	private Point endPoint;
+
+	public Line(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+
+	/**
+	 * 
+	 * @param endPoint
+	 * @param originPoint
+	 * @param fillColor
+	 */
+	public Line(Point endPoint, Point originPoint, int fillColor){
+
+	}
+
+	protected void calcSize(){
+
+	}
+
+	public void draw(){
+
+	}
+
+	public double getSize(){
+		return 0;
+	}
+
+}
