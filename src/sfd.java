04cb5050cf07b9a439420aaea2c8a2b584bb6e64
Andrew Scott <atscott01@gmail.com>
2012-11-28 10:38:06 -0600

    deleted file

--- src/sfd.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: scottat
- * Date: 11/28/12
- * Time: 10:34 AM
- * To change this template use File | Settings | File Templates.
- */
-public class sfd {
-}
