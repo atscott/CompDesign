e8c4b11361c3f85b3e1ea2ab1d46a23fe544832a
Andrew Scott <atscott01@gmail.com>
2012-12-05 10:48:49 -0600

    fin

--- src/lab2/Delta.java
+++ src/lab2/Delta.java
@@ -9,6 +9,8 @@ import java.util.Observer;
  * @created 05-Dec-2012 9:54:32 AM
  */
 public class Delta implements Observer, Displayable {
+    private GPSCoordinate lastCoordinate;
+    private GPSCoordinate currentCoordinate;
 
 	public Delta() {
 
@@ -16,13 +18,19 @@ public class Delta implements Observer, Displayable {
 
 	@Override
 	public void display() {
-
+        System.out.print("Change in X: " + currentCoordinate.getDeltaX(lastCoordinate));
+        System.out.print("Change in Y: " + currentCoordinate.getDeltaZ(lastCoordinate));
+        System.out.print("Change in Z: " + currentCoordinate.getDeltaZ(lastCoordinate));
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-
+        if(o instanceof GpsSubject){
+            GpsSubject subject = (GpsSubject)o;
+            lastCoordinate = currentCoordinate;
+            currentCoordinate = subject.getCoordinates();
+            display();
+        }
 	}
 
 }
