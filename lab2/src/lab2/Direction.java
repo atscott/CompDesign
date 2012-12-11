64d5ad0588ce966542c13c258b7a79191c5620e0
kt <ktohtz@gmail.com>
2012-12-10 18:06:40 -0600

    Added "Direction" class, bitches.

--- lab2/src/lab2/Direction.java
+++ lab2/src/lab2/Direction.java
@@ -4,25 +4,33 @@ import java.util.Observable;
 import java.util.Observer;
 
 /**
- * @author marius
+ * @author tohtz
  * @version 1.0
  * @created 05-Dec-2012 9:54:32 AM
  */
 public class Direction implements Observer, Displayable {
+    GpsSubject subject;
 
-	public Direction() {
-
+	public Direction(Observable o) {
+        subject = (GpsSubject)o;
 	}
 
 	@Override
 	public void display() {
-
+        if(subject != null){
+            System.out.println("Latitude:  " + subject.getCoordinates().getLatitude());
+            System.out.println("Longitude: " + subject.getCoordinates().getLongitude());
+            System.out.println("Elevation: " + subject.getCoordinates().getElevation());
+        }
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 
-
+       if(o != null) {
+           subject = (GpsSubject) o;
+       }
+       display();
 	}
 
 }
