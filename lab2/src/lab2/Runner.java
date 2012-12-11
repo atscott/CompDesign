6be300e5caa4fe0bc76a0277760c5356bbb0d45b
moorea <moorea@msoe.edu>
2012-12-10 20:21:50 -0600

    Update lab2/src/lab2/Runner.java

--- lab2/src/lab2/Runner.java
+++ lab2/src/lab2/Runner.java
@@ -1,46 +1,47 @@
 package lab2;
 
 import java.io.*;
-import java.lang.System;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-        public class Runner {
+public class Runner {
 
-            /**
-             *
-             * @param args
-             */
-            public static void main(String[] args) {
-                GpsSubject subject  = new GpsSubject();
+        /**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        GpsSubject subject  = new GpsSubject();
+        Delta delta = new Delta(subject);
+        Direction direction = new Direction(subject);
+        Altimeter altimeter = new Altimeter(subject);
 
+        for(GPSCoordinate coordinate : loadGPSFile()){
+            subject.setCoordinates(coordinate);
+        }
 
-                for(GPSCoordinate coordinate : loadGPSFile()){
-                    subject.setCoordinates(coordinate);
-                }
+	}
 
+    private static ArrayList<GPSCoordinate> loadGPSFile(){
+        ArrayList<GPSCoordinate> coordinates = new ArrayList<GPSCoordinate>();
+
+        //coordinate sets are separated by spaces
+        String[] coordinateSets = getFileAsString().split(" ");
+
+        for(String coordinateSet : coordinateSets){
+            //xyz values are separated by commas
+            String[] xyz = coordinateSet.split(",");
+            //if you don't have xyz, then bad coordinate set
+            if(xyz.length == 3){
+                coordinates.add(new GPSCoordinate(Double.parseDouble(xyz[0]), Double.parseDouble(xyz[1]), Double.parseDouble(xyz[2])));
+            }   else{
+                System.out.println("Invalid coordinate set: " + coordinateSet);
             }
+        }
 
-            private static ArrayList<GPSCoordinate> loadGPSFile(){
-                ArrayList<GPSCoordinate> coordinates = new ArrayList<GPSCoordinate>();
-
-                //coordinate sets are separated by spaces
-                String[] coordinateSets = getFileAsString().split(" ");
-
-                for(String coordinateSet : coordinateSets){
-                    //xyz values are separated by commas
-                    String[] xyz = coordinateSet.split(",");
-                    //if you don't have xyz, then bad coordinate set
-                    if(xyz.length == 3){
-                        coordinates.add(new GPSCoordinate(Double.parseDouble(xyz[0]), Double.parseDouble(xyz[1]), Double.parseDouble(xyz[2])));
-                    }   else{
-                        System.out.println("Invalid coordinate set: " + coordinateSet);
-                    }
-                }
-
-                return coordinates;
+        return coordinates;
     }
 
     private static String getFileAsString(){
